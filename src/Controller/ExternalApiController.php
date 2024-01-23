d502764242dea83d9680bcb37693f5d0fe427f9b
mashero <vyunov@softinvent.ru>
2024-01-23 08:42:09 +0300

    Добавлен слушатель изменения лотов, который чистит кеш

--- src/Controller/ExternalApiController.php
+++ src/Controller/ExternalApiController.php
@@ -3,16 +3,30 @@
 namespace App\Controller;
 
 use App\Repository\LotRepository;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 #[Route('/external/api')]
 class ExternalApiController extends AbstractController
 {
+    public const ALL_LOTS_CACHE_KEY = 'external-lots';
+
+    /**
+     * @throws InvalidArgumentException
+     * @throws ExceptionInterface
+     */
     #[Route('/lots', name: 'app_external_api', methods: ['GET'])]
-    public function getAllLots(LotRepository $repository): JsonResponse
+    public function getAllLots(LotRepository $repository, SerializerInterface $serializer, CacheInterface $oCache): JsonResponse
     {
-        return $this->json($repository->findAll());
+        $data = $oCache->get(self::ALL_LOTS_CACHE_KEY, function () use ($serializer, $repository) {
+            return $serializer->normalize($repository->findAll());
+        });
+
+        return $this->json($data);
     }
 }
--- src/Entity/Lot.php
+++ src/Entity/Lot.php
@@ -8,8 +8,10 @@ use App\Repository\LotRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
 
 #[ORM\Entity(repositoryClass: LotRepository::class)]
+#[HasLifecycleCallbacks]
 class Lot
 {
     #[ORM\Id]
